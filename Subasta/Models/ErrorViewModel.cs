6508c6ab344ff6bf7b30127a8f16e943886fa072
BerserkBlaze <49006400+BerserkBlaze@users.noreply.github.com>
2019-08-13 17:48:12 -0500

    Agregar archivos de proyecto.

---
+++ Subasta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Subasta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
